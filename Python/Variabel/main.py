00d1021859709ec51b3a4c8ebef87acd1f81f0b3
YordanProject <jordanahmad787@gmail.com>
2026-05-06 22:27:20 +0700

    Python Variable

---
+++ Python/Variabel/main.py
@@ -0,0 +1,22 @@
+# Variabel adalah tempat menyimpan data
+a = 10 # a adalah 10
+x = 5
+panjang = 1000
+
+print("Nilai a = ", a)
+print("Nilai x =  ", x)
+print("Nilai Panjang = ", panjang)
+
+# penamaan
+nilai_y = 15 #Menggunakan Underscore
+juta10 = 10000000 # Dengan angka
+nilaiZ = 17.5 # ini Boleh
+
+# Pemanggilan kedua
+print("Nilai a = ", a)
+a = 7
+print("a = ", a)
+
+# Assignment indirect
+b = a
+print("Nilai b = ", b)
